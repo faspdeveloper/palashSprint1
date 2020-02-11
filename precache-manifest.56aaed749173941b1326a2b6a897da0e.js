ddfc167ed50339219be160ac1c5a779c2f244c1d
palashnikam <nikampalash@gmail.com>
2020-02-11 17:18:54 +0530

    Updates

---
+++ precache-manifest.56aaed749173941b1326a2b6a897da0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c309fc58414a4a53176262d646a100",
+    "url": "/palashSprint1/index.html"
+  },
+  {
+    "revision": "1dbbb4a9b5b960505cde",
+    "url": "/palashSprint1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fe941cf85cf68de1a743",
+    "url": "/palashSprint1/static/js/2.d717271a.chunk.js"
+  },
+  {
+    "revision": "b85f3fe6317b66fabc4bc2dc9b8ca476",
+    "url": "/palashSprint1/static/js/2.d717271a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1dbbb4a9b5b960505cde",
+    "url": "/palashSprint1/static/js/main.3225b640.chunk.js"
+  },
+  {
+    "revision": "d5d1df7f508e7122dc4e",
+    "url": "/palashSprint1/static/js/runtime-main.68709639.js"
+  }
+]);
