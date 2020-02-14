d3292dd473b4b80fd0f24ac14455d9d329f00cc5
palashnikam <nikampalash@gmail.com>
2020-02-14 16:09:26 +0530

    Updates

---
+++ precache-manifest.34b8eaf688147e4c0d6744d35543cd29.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a3a7ca0974f5af7a87d4d22533c933",
+    "url": "/palashSprint1/index.html"
+  },
+  {
+    "revision": "17d4155c0629a3675f8d",
+    "url": "/palashSprint1/static/css/main.02c86b7a.chunk.css"
+  },
+  {
+    "revision": "2951e090170c85566e16",
+    "url": "/palashSprint1/static/js/2.2cea7fba.chunk.js"
+  },
+  {
+    "revision": "17543fc7a0ab69a6a1c82c6f28a7b760",
+    "url": "/palashSprint1/static/js/2.2cea7fba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17d4155c0629a3675f8d",
+    "url": "/palashSprint1/static/js/main.aac434fc.chunk.js"
+  },
+  {
+    "revision": "d5d1df7f508e7122dc4e",
+    "url": "/palashSprint1/static/js/runtime-main.68709639.js"
+  },
+  {
+    "revision": "68ed1dac06bf0409c18ae7bc62889170",
+    "url": "/palashSprint1/static/media/glyphicons-halflings-regular.68ed1dac.woff"
+  },
+  {
+    "revision": "7ad17c6085dee9a33787bac28fb23d46",
+    "url": "/palashSprint1/static/media/glyphicons-halflings-regular.7ad17c60.eot"
+  },
+  {
+    "revision": "e49d52e74b7689a0727def99da31f3eb",
+    "url": "/palashSprint1/static/media/glyphicons-halflings-regular.e49d52e7.ttf"
+  },
+  {
+    "revision": "ff423a4251cf2986555523dfe315c42b",
+    "url": "/palashSprint1/static/media/glyphicons-halflings-regular.ff423a42.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/palashSprint1/precache-manifest.da351d4335496ba8aec72b6705470cf1.js"
+  "/palashSprint1/precache-manifest.34b8eaf688147e4c0d6744d35543cd29.js"
 );
 
 self.addEventListener('message', (event) => {
