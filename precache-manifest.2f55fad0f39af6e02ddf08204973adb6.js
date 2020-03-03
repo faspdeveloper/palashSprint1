8d8dcd99747391a717cd48650beac378d3d6fc64
Dolly Chhabriya <dolly.c@altius.cc>
2020-03-03 23:33:07 +0530

    Updates

---
+++ precache-manifest.2f55fad0f39af6e02ddf08204973adb6.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362c04c0891e7e594f6000187d6af2ce",
+    "url": "/palashSprint1/index.html"
+  },
+  {
+    "revision": "3a4c90a07582db8c465f",
+    "url": "/palashSprint1/static/css/2.3564eac7.chunk.css"
+  },
+  {
+    "revision": "cd99b24f3ebf8a7b3a78",
+    "url": "/palashSprint1/static/css/main.9b8c0176.chunk.css"
+  },
+  {
+    "revision": "3a4c90a07582db8c465f",
+    "url": "/palashSprint1/static/js/2.297a2891.chunk.js"
+  },
+  {
+    "revision": "e9b0f08e51b1bc10808bfd55a56ae96b",
+    "url": "/palashSprint1/static/js/2.297a2891.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd99b24f3ebf8a7b3a78",
+    "url": "/palashSprint1/static/js/main.beb1332f.chunk.js"
+  },
+  {
+    "revision": "8d0a32efa86ea1a05536edead145e905",
+    "url": "/palashSprint1/static/js/main.beb1332f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5d1df7f508e7122dc4e",
+    "url": "/palashSprint1/static/js/runtime-main.68709639.js"
+  },
+  {
+    "revision": "5f387a445a8947c4b6075521f849d884",
+    "url": "/palashSprint1/static/media/bg.5f387a44.png"
+  },
+  {
+    "revision": "19ee6b7e6642d75d6144b0c8209c93d6",
+    "url": "/palashSprint1/static/media/error.19ee6b7e.svg"
+  },
+  {
+    "revision": "4f0022f25672c7f501c339cbf98d9117",
+    "url": "/palashSprint1/static/media/fontawesome-webfont.4f0022f2.ttf"
+  },
+  {
+    "revision": "776d58f453c8fe5d6a89e9c31ee223ff",
+    "url": "/palashSprint1/static/media/fontawesome-webfont.776d58f4.svg"
+  },
+  {
+    "revision": "90186830c9c50a0fed932494581761d9",
+    "url": "/palashSprint1/static/media/fontawesome-webfont.90186830.eot"
+  },
+  {
+    "revision": "fdf491ce5ff5b2da02708cd0e9864719",
+    "url": "/palashSprint1/static/media/fontawesome-webfont.fdf491ce.woff"
+  },
+  {
+    "revision": "68ed1dac06bf0409c18ae7bc62889170",
+    "url": "/palashSprint1/static/media/glyphicons-halflings-regular.68ed1dac.woff"
+  },
+  {
+    "revision": "7ad17c6085dee9a33787bac28fb23d46",
+    "url": "/palashSprint1/static/media/glyphicons-halflings-regular.7ad17c60.eot"
+  },
+  {
+    "revision": "e49d52e74b7689a0727def99da31f3eb",
+    "url": "/palashSprint1/static/media/glyphicons-halflings-regular.e49d52e7.ttf"
+  },
+  {
+    "revision": "ff423a4251cf2986555523dfe315c42b",
+    "url": "/palashSprint1/static/media/glyphicons-halflings-regular.ff423a42.svg"
+  },
+  {
+    "revision": "d505833956ebd4b94d1afab202118b92",
+    "url": "/palashSprint1/static/media/login-screen.d5058339.jpg"
+  },
+  {
+    "revision": "2f50af2c93c75ffb9664b8d1d1aeae6e",
+    "url": "/palashSprint1/static/media/logo-small.2f50af2c.png"
+  },
+  {
+    "revision": "7f5c043ffac167ae698b863abc71f1fa",
+    "url": "/palashSprint1/static/media/logo.7f5c043f.png"
+  },
+  {
+    "revision": "21dfa3149b274acb9c1819d342a6a169",
+    "url": "/palashSprint1/static/media/play-pause.21dfa314.svg"
+  },
+  {
+    "revision": "541c5908330894e482ace73c6613678a",
+    "url": "/palashSprint1/static/media/spritemap.541c5908.png"
+  },
+  {
+    "revision": "863f74c11fcfa21afd1d229a932f2780",
+    "url": "/palashSprint1/static/media/spritemap@2x.863f74c1.png"
+  },
+  {
+    "revision": "9b737958b1644b46b23904e53afcac50",
+    "url": "/palashSprint1/static/media/video-play.9b737958.svg"
+  },
+  {
+    "revision": "1716558842d1d058da9e818e1b1e59d5",
+    "url": "/palashSprint1/static/media/wall_1.17165588.jpg"
+  }
+]);
